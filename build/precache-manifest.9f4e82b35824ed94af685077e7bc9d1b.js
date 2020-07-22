811cf8f3c55f349747e36343f0b84d8bfe9f6353
Naman Aggarwal <naman.aggarwal2001@gmail.com>
2020-07-22 14:44:00 +0530

    changes in index.html

---
+++ build/precache-manifest.9f4e82b35824ed94af685077e7bc9d1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92554416c1778432054cb662c89b628a",
+    "url": "/Keeper/index.html"
+  },
+  {
+    "revision": "8f3c7ee552ebf69cb0e9",
+    "url": "/Keeper/static/js/2.d5d55834.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/Keeper/static/js/2.d5d55834.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ed7e2b1a161989e839",
+    "url": "/Keeper/static/js/main.091e62c9.chunk.js"
+  },
+  {
+    "revision": "2df5c4b43b55ed35b1bb",
+    "url": "/Keeper/static/js/runtime-main.7da4a144.js"
+  }
+]);
